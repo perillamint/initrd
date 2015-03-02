81de12cbbafa4f28e1499259bf502f7338010090
perillamint <perillamint@gmail.com>
2015-03-02 19:20:44 +0900

    Dir auto generation.

--- build-initramfs.sh
+++ build-initramfs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd /usr/src/initramfs
+
+for dir in "dev proc sys" do
+    if [ -d $dir ]
+        mkdir $dir
+    fi
+done
+
 lddtree --copy-to-tree=initramfs $(cat elflist)
-cd initramfs
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initrd.img
