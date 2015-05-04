5e850def059dd02bc2965a24f490197de4b55554
perillamint <perillamint@gmail.com>
2015-05-04 16:22:35 +0900

    Fix for loop

--- build-initramfs.sh
+++ build-initramfs.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-cd /usr/src/initramfs
+cd /usr/src/initrd/initramfs
 
-for dir in "dev proc sys" do
-    if [ -d $dir ]
+for dir in dev proc sys mnt mnt/root usr usr/sbin usr/bin usr/lib do
+    if [ ! -d $dir ]
         mkdir $dir
     fi
 done
 
+cd ..
+
 lddtree --copy-to-tree=initramfs $(cat elflist)
+
+cd initramfs
+
 find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initrd.img
